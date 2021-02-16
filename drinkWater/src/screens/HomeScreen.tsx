d516b807773750bae51f499bc2a3ab5c09a4e003
Ola <alk.pikus@gmail.com>
2021-02-16 19:42:17 +0100

    feat: created screen component

---
+++ drinkWater/src/screens/HomeScreen.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Button from '../components/Button';
+import { AppState } from '../reducers';
+import { setReminderTime } from '../reducers/reminderReducer';
+import { AppDispatch } from '../store';
+import { setNotification } from '../utils/setNotification';
+
+const HomeScreen = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const reminderTime = useSelector<AppState, number>(
+    (state) => state.reminder.timeSet,
+  );
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    setCurrentTime(new Date().getMilliseconds());
+  }, []);
+
+  const setNewNotification = () => {
+    dispatch(setReminderTime());
+    setNotification();
+  };
+  const isDisabled = currentTime < reminderTime ? true : false;
+
+  return (
+    <View style={styles.container}>
+      <Button
+        onPress={setNewNotification}
+        title="Firebase Cloud Messaging"
+        type="reminder"
+        disabled={isDisabled}
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    textAlign: 'center',
+    backgroundColor: '#307ecc',
+  },
+});
+export default HomeScreen;
