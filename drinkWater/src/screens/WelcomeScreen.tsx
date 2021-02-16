43a1f35ce69928fafe4e4809ffc6303540b35448
Ola <alk.pikus@gmail.com>
2021-02-16 19:45:53 +0100

    refactor: updated WelcomeScreen

--- drinkWater/src/screens/WelcomeScreen.tsx
+++ drinkWater/src/screens/WelcomeScreen.tsx
@@ -1,17 +1,39 @@
 import { useNavigation } from '@react-navigation/native';
-import auth from '@react-native-firebase/auth';
-import React, { useEffect } from 'react';
-import { View } from 'react-native';
+import React from 'react';
+import { ActivityIndicator, StyleSheet } from 'react-native';
+import { TouchableHighlight } from 'react-native-gesture-handler';
+import { useSelector } from 'react-redux';
+
+import { AppState } from '../reducers';
 
 const WelcomeScreen = () => {
   const navigation = useNavigation();
-  useEffect(() => {
-    auth().onAuthStateChanged((user) => {
-      navigation.navigate(user ? 'Main' : 'SignUp');
-    });
-  }, []);
-
-  return <View style={{ backgroundColor: 'red', height: 200 }} />;
+  const isLogged = useSelector<AppState, boolean>((state) => state.auth.isAuth);
+  const isLoading = useSelector<AppState, boolean>(
+    (state) => state.auth.isLoading,
+  );
+  return (
+    <>
+      {isLoading && <ActivityIndicator style={styles.indictator} />}
+      <TouchableHighlight
+        style={{ backgroundColor: 'red', height: 200 }}
+        onPress={() => {
+          navigation.navigate(isLogged ? 'Home' : 'SignIn');
+        }}
+      />
+    </>
+  );
 };
-
+const styles = StyleSheet.create({
+  indictator: {
+    alignItems: 'center',
+    bottom: 0,
+    justifyContent: 'center',
+    left: 0,
+    opacity: 1,
+    position: 'absolute',
+    right: 0,
+    top: 0,
+  },
+});
 export default WelcomeScreen;
